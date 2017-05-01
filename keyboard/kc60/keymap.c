9fa4a40b02775797d8aa1baee3319c836ed7a2b7
Joakim Erelt <joakim.erelt@gmail.com>
2017-05-01 17:43:42 +0200

    Map caps-lock to esc

--- keyboard/kc60/keymap.c
+++ keyboard/kc60/keymap.c
@@ -3,13 +3,13 @@
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     KEYMAP_ANSI(
-        ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
+        GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
-        CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
+        ESC, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,  \
         LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,          RSFT, \
         LCTL,LALT,LGUI,          SPC,                     NO,  FN0, NO,  RCTL),
     KEYMAP_ANSI(
-         GRV,  F1,  F2,  F3,  F4,  F5,  F6,   F7,  F8,  F9, F10, F11, F12,TRNS, \
+         ESC,  F1,  F2,  F3,  F4,  F5,  F6,   F7,  F8,  F9, F10, F11, F12,TRNS, \
         TRNS,TRNS,  UP,TRNS,TRNS,TRNS,TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
         TRNS,LEFT,DOWN,RGHT,TRNS,TRNS,TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS, \
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, TRNS,TRNS,TRNS,TRNS,          TRNS, \
