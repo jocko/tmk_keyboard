810f1a277c7e99e4965b3a722ce96998ee79b347
Joakim Erelt <joakim.erelt@gmail.com>
2017-05-14 19:19:32 +0200

    Add LALT tap key

--- keyboard/traktor/keymap.c
+++ keyboard/traktor/keymap.c
@@ -8,7 +8,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,    KC_BSPC},
     {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN, KC_ENT},
     {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, KC_RSFT},
-    {KC_NO,   KC_NO,   KC_NO,   KC_LALT, KC_FN0,  KC_LCTL, KC_SPC,  KC_FN1,  KC_FN8,  KC_NO,    KC_NO,   KC_NO},
+    {KC_NO,   KC_NO,   KC_NO,   KC_FN7,  KC_FN0,  KC_LCTL, KC_SPC,  KC_FN1,  KC_FN8,  KC_NO,    KC_NO,   KC_NO},
   },
   [1] = {
     {KC_FN5,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,    KC_TRNS},
@@ -38,9 +38,9 @@ const action_t PROGMEM fn_actions[] = {
 
     [5] = ACTION_MACRO(CMD_TAB),
     [6] = ACTION_MACRO(CTL_TAB),
-    [7] = ACTION_MODS_TAP_KEY(MOD_RGUI, KC_SCLN),
+    [7] = ACTION_MODS_TAP_KEY(MOD_LALT, KC_F5),
     [8] = ACTION_MODS_ONESHOT(MOD_RGUI),
-    [9] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_BSLS),
+    /* [9] = ACTION_MODS_TAP_KEY(KC_RSFT, KC_CAPS), */
 
     [10] = ACTION_MODS_KEY(MOD_LSFT, KC_GRV),
     [11] = ACTION_MODS_KEY(MOD_LSFT, KC_QUOT),
